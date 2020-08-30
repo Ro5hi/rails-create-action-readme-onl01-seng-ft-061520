cf21e3623bc6e1dad191bc028a5e0d43057da1bb
Shirlen Detablan <slen.detablan@gmail.com>
2020-08-30 23:02:07 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
 
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+  
 end
